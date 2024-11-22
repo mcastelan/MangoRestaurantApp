1f98c730bcb9c03f93f8df483c91adccd8a341b5
Marco Castelan <marcocastelan@gmail.com>
2024-11-22 10:10:38 -0600

    mend

--- Integration/Mango.MessageBus/IMessageBus.cs
+++ Integration/Mango.MessageBus/IMessageBus.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace Mango.MessageBus
 {
     public interface IMessageBus
-    {
+    { 
         Task PublishMessage(BaseMessage message, string topicName);
 
     }
